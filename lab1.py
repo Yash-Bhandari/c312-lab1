8cf5320a289b1c3c5644d2a1f96eb64f01fb4a83
Yash Bhandari <yash@bhandari.dev>
2022-09-14 16:03:47 -0600

    added stupid simple rectangle

--- lab1.py
+++ lab1.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import LargeMotor, MoveSteering, OUTPUT_A, OUTPUT_D
+from ev3dev2.motor import LargeMotor, MoveSteering, OUTPUT_A, OUTPUT_D, MoveTank
 from time import sleep
 import sys
 
 steer_pair = MoveSteering(OUTPUT_D, OUTPUT_A, motor_class=LargeMotor)
+tank = MoveTank(OUTPUT_D, OUTPUT_A, motor_class=LargeMotor)
 
 def debug_print(*args, **kwargs):
     '''Print debug messages to stderr.
@@ -47,12 +48,16 @@ def lemniscate():
     # for step in range(num_steps):
     #     steer_pair.on(steering=step * max_steer / num_steps, speed=30)
     #     sleep(0.5)
+
+def turn_90_degrees():
+    steer_pair.on_for_seconds(steering=90, speed=10, seconds=1.9)
+
 def rectangle():
     for step in range(4):
         debug_print('step {}'.format(step))
-        steer_pair.on_for_seconds(steering=0, speed=30, seconds=5)
-        steer_pair.on_for_seconds(steering=90, speed=20, seconds=1)
+        steer_pair.on_for_seconds(steering=0, speed=30, seconds=3)
+        turn_90_degrees()
 
-lemniscate()
+# lemniscate()
 # rectangle()
 # steer_pair.on_for_seconds(steering=100, speed=50, seconds=10, block=True)
